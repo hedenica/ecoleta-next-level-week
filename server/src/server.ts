557d26fb1f57b8dde3e643b307bc132dd7a664d7
hedenica <hedenica@hotmail.com>
2020-06-04 20:12:05 -0300

    🎉 first commit

---
+++ server/src/server.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import routes from './routes';
+
+const app = express();
+
+// forma do express entender o corpo da requisição em formato JSON.
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads'))); 
+
+// Request Params: Parâmetros que vem na própria rota que identificam um recurso
+// Query Param: Parâmetros que vem na propria rota, geralmente opcionais para filtros, paginação...
+// Request BOdy: Parâmetros para criação e atualização de informações
+
+
+app.listen(3333);
