5cc98963c921c761d4fdd28f996d2c9868d0d3c9
hedenica <hedenica@hotmail.com>
2020-06-06 12:06:49 -0300

    ✨  send mail and whatsapp via app

--- mobile/src/pages/Detail/index.tsx
+++ mobile/src/pages/Detail/index.tsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { Feather as Icon, FontAwesome } from '@expo/vector-icons';
-import { View, Text, StyleSheet, TouchableOpacity, Image, SafeAreaView } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Image, SafeAreaView, Linking } from 'react-native';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import { RectButton } from 'react-native-gesture-handler';
 import api from '../../services/api';
+import * as MailComposer from 'expo-mail-composer';
+
 
 interface Params {
   point_id: number;
@@ -40,6 +42,18 @@ const Detail = () => {
     navigation.goBack();
   }
 
+  function handleWhatsapp() {
+    Linking.openURL(`whatsapp://send?phone=${+5511952404398}&text=♻️ Tenho interesse em solicitar uma coleta`);
+  }
+
+  function handleComposeMail() {
+    MailComposer.composeAsync({
+      subject: '♻️ Solicitação de coleta de resíduos',
+      recipients: [data.point.email],
+      body: '🚛 Olá, gostaria de solicitar uma coleta!'
+    })
+  }
+
   if (!data.point) {
     return null;
   }
@@ -63,12 +77,12 @@ const Detail = () => {
         </View>
       </View>
       <View style={styles.footer}>
-        <RectButton style={styles.button} onPress={() => {}}>
+        <RectButton style={styles.button} onPress={handleWhatsapp}>
           <FontAwesome name="whatsapp" size={20} color="#FFFF" />
           <Text style={styles.buttonText}>Whatsapp</Text>
         </RectButton>
 
-        <RectButton style={styles.button} onPress={() => {}}>
+        <RectButton style={styles.button} onPress={handleComposeMail}>
           <Icon name="mail" size={20} color="#FFFF" />
           <Text style={styles.buttonText}>E-mail</Text>
         </RectButton>
