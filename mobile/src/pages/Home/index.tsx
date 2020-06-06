0f7ab105d1750266f8de80633738e505994fc59c
hedenica <hedenica@hotmail.com>
2020-06-05 21:35:11 -0300

    ✨  home page completed

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,11 +1,35 @@
 import React from 'react';
-import { View, Image, StyleSheet } from 'react-native';
+import { Feather as Icon } from '@expo/vector-icons';
+import { View, ImageBackground, Text, Image, StyleSheet } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
 
 const Home = () => {
   return (
-    <View style={styles.container}>
-      <Image source={require('../../assets/logo.png')}/>
-    </View>
+    <ImageBackground 
+      source={require('../../assets/home-background.png')} 
+      style={styles.container}
+      imageStyle={{ width: 274, height: 368 }}
+      
+    >
+      <View style={styles.main}>
+        <Image source={require('../../assets/logo.png')}/>
+        <Text style={styles.title}>Seu marketplace de coleta de resíduos</Text>
+        <Text style={styles.description}>Ajudamos pessoas a encontrarem pontos de coleta de forma de eficiente</Text>
+      </View>
+
+      <View style={styles.footer}>
+        <RectButton style={styles.button} onPress={() => {}}>
+          <View style={styles.buttonIcon}>
+            <Text> 
+              <Icon name="arrow-right" color="#fff" size={24} /> 
+            </Text>
+          </View>
+        <Text style={styles.buttonText}>
+          Entrar
+        </Text>
+        </RectButton>
+      </View>
+    </ImageBackground>
   )
 };
 
@@ -13,6 +37,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 32,
+    backgroundColor: '#f0f0f5'
   },
 
   main: {
