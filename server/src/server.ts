5141b543bed59701747fc72e935772bb21b56af8
hedenica <hedenica@hotmail.com>
2020-06-07 14:17:51 -0300

    ✨  adding validation using Celebrate and Joi

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import path from 'path';
 import routes from './routes';
+import { errors } from 'celebrate';
 
 const app = express();
 
@@ -19,5 +20,6 @@ app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 // Query Param: Parâmetros que vem na propria rota, geralmente opcionais para filtros, paginação...
 // Request BOdy: Parâmetros para criação e atualização de informações
 
+app.use(errors());
 
 app.listen(3333);
